514ebe44fe40b0e68c6431aafa6f1f0766dc6563
Anastasiya2808 <anas2210@mail.ru>
2022-06-12 19:25:23 +0300

    Initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Модульный тест для простого приложения.
+ */
+public class AppTest {
+
+    /**
+     * Строгий тест
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
